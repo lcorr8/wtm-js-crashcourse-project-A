14f39c5e75d65ffd162317d05641dbd3639280cb
lcorr8 <l.corr.v@gmail.com>
2019-10-29 14:36:21 +0100

    save function prevents duplicate entries in db, no need to delete json files every time index.js is ran :P

--- database.js
+++ database.js
@@ -1,41 +1,52 @@
 const fs = require('fs');
 
-const save = function (filename, data) {
+function load(filename) {
+  return JSON.parse(fs.readFileSync(filename, 'utf8'));
+}
+
+function getById(id, elements) {
+  return elements.find((element) => element.id === id);
+}
+
+function updateEntry(filename, id, key, value) {
+  const data = this.load(filename);
+  const elementIndex = data.findIndex((element) => element.id === id);
+  data[elementIndex][key] = value;
+  fs.writeFileSync(filename, JSON.stringify(data, null, 2));
+}
+
+function updateObject(filename, id, updatedObj) {
+  const data = load(filename);
+  const elementIndex = data.findIndex((element) => element.id === id);
+  data[elementIndex] = updatedObj;
+  fs.writeFileSync(filename, JSON.stringify(data, null, 2));
+}
+
+function save(filename, data) {
   // no file or file exists and its empty
   if (!fs.existsSync(filename) || (fs.existsSync(filename) && fs.readFileSync(filename, 'utf8').length === 0)) {
     // add the initial array
     fs.writeFileSync(filename, JSON.stringify([])); // , null, 2
   }
+
   // add the first object
-  if (fs.existsSync(filename) && fs.readFileSync(filename, 'utf8').length >= 2) {
-    const dbdata = fs.readFileSync(filename, 'utf8');
-    const json = JSON.parse(dbdata);
+  if (fs.existsSync(filename) && fs.readFileSync(filename, 'utf8').length === 2) {
+    const json = load(filename);
     json.push(data);
-
     fs.writeFileSync(filename, JSON.stringify(json, null, 2));
+  // if more than one object exists
+  } else if (fs.existsSync(filename) && fs.readFileSync(filename, 'utf8').length > 2) {
+    // check if object exists by id update otherwise just add
+    const json = load(filename);
+    if (getById(data.id, json) === undefined) {
+      json.push(data);
+      fs.writeFileSync(filename, JSON.stringify(json, null, 2));
+    } else {
+      updateObject(filename, data.id, data);
+    }
   }
-  /**
-   * TODO: handle checking if object already exists by id,
-   * so it doesn't get added again when running multiple times w/o having to delete json file. */
-};
-const load = function (filename) {
-  return JSON.parse(fs.readFileSync(filename, 'utf8'));
-};
-
-const updateEntry = function (filename, id, key, value) {
-  const data = this.load(filename);
-  const elementIndex = data.findIndex((element) => element.id === id);
-  data[elementIndex][key] = value;
-  fs.writeFileSync(filename, JSON.stringify(data, null, 2));
-};
-
-const updateObject = function (filename, id, updatedObj) {
-  const data = this.load(filename);
-  const elementIndex = data.findIndex((element) => element.id === id);
-  data[elementIndex] = updatedObj;
-  fs.writeFileSync(filename, JSON.stringify(data, null, 2));
-};
+}
 
 module.exports = {
-  save, load, updateEntry, updateObject,
+  load, getById, updateEntry, updateObject, save,
 };
