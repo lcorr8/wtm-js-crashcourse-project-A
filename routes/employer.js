7969cb8dc7f3a6622d2ad671798eca5feea82c68
lcorr8 <l.corr.v@gmail.com>
2019-11-15 23:16:37 +0100

    add catch errors to crud routes

--- routes/employer.js
+++ routes/employer.js
@@ -9,25 +9,25 @@ router.get('/employer/all', async (req, res) => {
   res.render('employers', { employers });
 });
 router.get('/employer/all/json', async (req, res) => {
-  const employers = await EmployerService.findAll();
+  const employers = await EmployerService.findAll().catch((err) => console.log(err));
   res.send(employers);
 });
 // axios.get('/employer/all').then(console.log);
 
 router.get('/employer/:id', async (req, res) => {
-  const employer = await EmployerService.find({ _id: req.params.id });
+  const employer = await EmployerService.find({ _id: req.params.id }).catch((err) => console.log(err));
   // res.send(employer);
   res.render('employer', { employer });
 });
 router.get('/employer/:id/json', async (req, res) => {
-  const employer = await EmployerService.find({ _id: req.params.id });
+  const employer = await EmployerService.find({ _id: req.params.id }).catch((err) => console.log(err));
   res.send(employer);
 });
 // axios.get('/employer/:id').then(console.log);
 
 // create new
 router.post('/employer', async (req, res) => {
-  const employer = await EmployerService.add(req.body);
+  const employer = await EmployerService.add(req.body).catch((err) => console.log(err));
   res.send(employer);
   console.log(req.body);
 });
@@ -35,14 +35,14 @@ router.post('/employer', async (req, res) => {
 
 // update
 router.put('/employer/:id', async (req, res) => {
-  const employer = await EmployerService.updateOne(req.params.id, req.body);
+  const employer = await EmployerService.updateOne(req.params.id, req.body).catch((err) => console.log(err));
   res.send(employer);
   console.log(req.body);
 });
 // axios.put('/employer/:id', { email: 'HeadMaster@hogwarts.edu' }).then(console.log);
 
 router.delete('/employer/:id', async (req, res) => {
-  const employer = await EmployerService.deleteOne({ _id: req.params.id });
+  const employer = await EmployerService.deleteOne({ _id: req.params.id }).catch((err) => console.log(err));
   res.send(employer);
   console.log(req.body);
 });
--- routes/notification.js
+++ routes/notification.js
@@ -3,31 +3,31 @@ const router = express.Router();
 const NotificationService = require('../services/notification-service');
 
 router.get('/notification/all', async (req, res) => {
-  const notifications = await NotificationService.findAll();
+  const notifications = await NotificationService.findAll().catch((err) => console.log(err));
   // res.send(notifications);
   res.render('notifications', { notifications });
 });
 
 router.get('/notification/:id', async (req, res) => {
-  const notification = await NotificationService.find({ _id: req.params.id });
+  const notification = await NotificationService.find({ _id: req.params.id }).catch((err) => console.log(err));
   // res.send(notification)
   res.render('notification', { notification });
 });
 
 router.post('/notification', async (req, res) => {
-  const notification = await NotificationService.add(req.body);
+  const notification = await NotificationService.add(req.body).catch((err) => console.log(err));
   res.send(notification);
   console.log(req.body);
 });
 
 router.put('/notification/:id', async (req, res) => {
-  const notification = await NotificationService.updateOne(req.params.id, req.body);
+  const notification = await NotificationService.updateOne(req.params.id, req.body).catch((err) => console.log(err));
   res.send(notification);
   console.log(req.body);
 });
 
 router.delete('/notification/:id', async (req, res) => {
-  const notification = await NotificationService.deleteOne({ _id: req.params.id });
+  const notification = await NotificationService.deleteOne({ _id: req.params.id }).catch((err) => console.log(err));
   res.send(notification);
 });
 
